67a8ff48435cb6fb78be91b8c8eb572d32e600bf
Ilya-kuimov <71551803+Ilya-kuimov@users.noreply.github.com>
2020-10-10 20:56:53 +0300

    Update generate_md.py

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,3 +1,12 @@
-out.txt
+z=open('intervals.md','a')
+z.write("#Inervals\n")
+x=open('source_leetcode_data.txt')
+
+for line in x in range(0,2):
+ 
+  z.write('#'+line)
+
+  
+readline(0)
 +_[Insert Interval](#insert-interval)
   
