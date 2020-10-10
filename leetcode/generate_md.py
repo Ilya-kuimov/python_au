d65feb5f44f559c2df5b54fb8e1efe57dbd1cbb3
Ilya-kuimov <71551803+Ilya-kuimov@users.noreply.github.com>
2020-10-10 22:30:29 +0300

    Update generate_md.py

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,14 +1,21 @@
-z=open('intervals.md','a')
-z.write("#Inervals\n")
-x=open('source_leetcode_data.txt')
-for n, line in enumerate(source_leetcode_data.txt)
-  if n = 0:
-  z.write('##'+line)
-  elif n=1:
-   z.write(line)
-   elif n>1:
-    z.write('python\n'+line)
-  
-readline(0)
-+_[Insert Interval](#insert-interval)
-  
+Class Solution:
+    def f(self, title, link, code):
+        self.title = title
+        self.link = link
+        self.code = code
+    def d(self, source_lines):
+        title = source_lines[0].split(". ")[1][:-1]
+        return title
+    def c(self, source_lines):
+        link = source_lines[1].split("/")[-2]
+        return link
+    def v(self, source_lines):
+        code = source_lines[3::]
+        new_code = ""
+        for new_line in code:
+            new_code += new_line
+        new_code = "```python\n" + new_code + "\n```"
+        return new_code
+    def m(self, source_lines):
+        leetlink = source_lines[1]
+        return leetlink
