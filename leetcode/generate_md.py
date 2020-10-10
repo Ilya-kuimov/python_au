99e3b55fb81cdf2c62d13f8aaf353e557568968b
Ilya-kuimov <71551803+Ilya-kuimov@users.noreply.github.com>
2020-10-11 01:57:09 +0300

    Update generate_md.py

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,47 +1,59 @@
-class Solution:
+class MdSource:
     def __init__(self, title, link, code):
         self.title = title
         self.link = link
         self.code = code
 
-    def get_md_title(self, source_lines):
-        title = source_lines[0].split(". ")[1][:-1]
-        return title
+    def get_md_title(self):
+        return "## " + self.title.split(". ")[1].strip("\n")
 
-    def get_md_link(self, source_lines):
-        link = source_lines[1].split("/")[-2]
-        return link
+    def get_md_link(self):
+        title = self.title.split(". ")[1].strip("\n")
+        lnk_tale = self.link.split("/")[-2]
+        return f"+ [{title}](#{lnk_tale})"
 
-    def get_md_formatsolution(self, source_lines):
-        code = source_lines[3::]
-        new_code = ""
-        for new_line in code:
-            new_code += new_line
-        new_code = "```python\n" + new_code + "\n```"
-        return new_code
+    def get_md_code(self):
+        return f"```python\n{self.code}\n```\n"
 
-    def get_Leetcodelink(self, source_lines):
-        leetlink = source_lines[1]
-        return leetlink
+    def get_md_task(self):
+        return f"{self.get_md_title()}\n\n{self.link}\n\n{self.get_md_code()}\n"
 
-with open("source_leetcode_data.txt", 'r') as in_file:
-    source_lines = in_file.readlines()
-title = source_lines[0].split(". ")[:-1]
-link = source_lines[1].split("/")[-2]
-code = source_lines[3::]
-leetlink = source_lines[1]
-plus, other = '', ''
-with open("intervals.md", 'r') as in_file:
-    source_lines1 = in_file.readlines()
-for i in range(1, len(source_lines1)):
-    if source_lines1[i][0] == "+":
-        plus += source_lines1[i]
-    else:
-        other += source_lines1[i]
-out_file = open("intervals.md", 'w')
-out_file.write("# {}\n\n{}".format("Intervals", plus))
-out_file.write("+[{}](#{}){}\n".format(Solution.get_md_title(title, source_lines), Solution.get_md_link(link, source_lines), other))
-out_file.write("\n## {}\n\n".format(Solution.get_md_title(title, source_lines)))
-out_file.write("{}\n".format((Solution.get_Leetcodelink(leetlink, source_lines))))
-out_file.write("{}".format(Solution.get_md_formatsolution(code, source_lines)))
-out_file.close()
+def read_txt():
+    with open("in.txt") as f:
+        data = f.readlines()
+    res = [data[0], data[1].strip("\n"), "".join(map(lambda x: x[8::], data[6::]))]
+    return res
+
+def read_md(filename):
+    with open(f"{filename}", "r") as f:
+        data = f.read()
+    return data
+
+def prepare_data_to_write(md_obj, data):
+    links, tasks = '', ''
+    if len(data) != 0:
+        links, tasks = data.split("<!---->\n")
+
+    links += md_obj.get_md_link()
+    tasks += md_obj.get_md_task()
+    tasks.strip('\n')
+
+    return [links, tasks]
+
+def write_to_file(filename, data):
+    header = f"# {filename.split('.')[0].upper()}\n\n"
+    with open(f"{filename}", "w") as f:
+        if header not in data[0]:
+            f.write(header)
+        f.write(f"{data[0]}")
+        f.write("\n<!---->\n")
+        f.write(data[1])
+
+def main():
+    md = MdSource(*read_txt())
+    s = read_md('lists.md')
+    s = prepare_data_to_write(md, s)
+    write_to_file('lists.md', s)
+
+if __name__ == "__main__":
+    main()
