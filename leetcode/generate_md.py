aaedaafa67456694c01c7119ceb0ca2abe40a1ec
Ilya-kuimov <71551803+Ilya-kuimov@users.noreply.github.com>
2020-10-11 02:00:24 +0300

    Update generate_md.py

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,59 +1,45 @@
-class MdSource:
-    def __init__(self, title, link, code):
-        self.title = title
-        self.link = link
-        self.code = code
+def GetName(all_lines):
+    name = all_lines[0]
+    name_list = list(name)
+    name_list[0] = ''
+    i = 1
+    while name_list[i - 1] != '.':
+        name_list[i - 1] = ''
+        i += 1
+    dot_place = name.find('.')
+    name_list[dot_place] = ''
+    name.strip()
+    name = ''.join(name_list)
+    name1 = "-".join(name.split())
+    return [name, name1]
 
-    def get_md_title(self):
-        return "## " + self.title.split(". ")[1].strip("\n")
 
-    def get_md_link(self):
-        title = self.title.split(". ")[1].strip("\n")
-        lnk_tale = self.link.split("/")[-2]
-        return f"+ [{title}](#{lnk_tale})"
+def GetUrl(all_lines):
+    return all_lines[1]
 
-    def get_md_code(self):
-        return f"```python\n{self.code}\n```\n"
 
-    def get_md_task(self):
-        return f"{self.get_md_title()}\n\n{self.link}\n\n{self.get_md_code()}\n"
+def GetCode(all_lines):
+    print('```python', file=fout)
+    for i in range(2, len(all_lines)):
+        print(all_lines[i], file=fout)
+    print('```', file=fout)
 
-def read_txt():
-    with open("in.txt") as f:
-        data = f.readlines()
-    res = [data[0], data[1].strip("\n"), "".join(map(lambda x: x[8::], data[6::]))]
-    return res
 
-def read_md(filename):
-    with open(f"{filename}", "r") as f:
-        data = f.read()
-    return data
+def PrintMarkDown(all_lines):
+    for i in range(len(all_linked_lines)):
+        if all_linked_lines[i] == '':
+            all_linked_lines.insert(i, '+ [{}](#{})'.format(GetName(all_lines)[0], GetName(all_lines)[1]))
+            break
+    for i in all_linked_lines:
+        print(i, file=fout)
+    print('## ' + str(GetName(all_lines)[0]), file=fout)
+    print(GetUrl(all_lines), file=fout)
+    GetCode(all_lines)
 
-def prepare_data_to_write(md_obj, data):
-    links, tasks = '', ''
-    if len(data) != 0:
-        links, tasks = data.split("<!---->\n")
 
-    links += md_obj.get_md_link()
-    tasks += md_obj.get_md_task()
-    tasks.strip('\n')
-
-    return [links, tasks]
-
-def write_to_file(filename, data):
-    header = f"# {filename.split('.')[0].upper()}\n\n"
-    with open(f"{filename}", "w") as f:
-        if header not in data[0]:
-            f.write(header)
-        f.write(f"{data[0]}")
-        f.write("\n<!---->\n")
-        f.write(data[1])
-
-def main():
-    md = MdSource(*read_txt())
-    s = read_md('lists.md')
-    s = prepare_data_to_write(md, s)
-    write_to_file('lists.md', s)
-
-if __name__ == "__main__":
-    main()
+with open("source_leetcode_data.txt", 'r') as fin:
+    all_lines = fin.read().splitlines()
+with open('linked-list.md', 'r') as fin:
+    all_linked_lines = fin.read().splitlines()
+fout = open("linked-list.md", "w")
+PrintMarkDown(all_lines)
